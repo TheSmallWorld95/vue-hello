1a7eb996fbd008a16e9b7ac2f3479d5f7802df30
Fabio-Colombo <thesmallworld95@gmail.com>
2021-03-23 16:42:51 +0100

    Bonus Completo

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,13 +1,19 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 var app = new Vue(
   {
     el: '#root',
     data: {
       message: "Hello World",
       how: "How are you?",
+      img: "https://picsum.photos/200/300",
+      getImage: "",
       red: "red",
       blue: "blue",
-      colorChange: "purple"
+      colorChange: "purple",
+      name: "",
+      surname: "",
     },
     methods: {
       changeColor: function () {
@@ -16,7 +22,17 @@ var app = new Vue(
         } else {
           this.colorChange = "purple"
         }
+      },
+      Image: function () {
+        if (this.getImage == "") {
+          this.getImage = "https://picsum.photos/200/300"
+        } else {
+          this.getImage = ""
+        }
       }
+      // transfer: function () {
+      //   this.name =
+      // }
     }
   }
 );
